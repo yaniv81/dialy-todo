8f3d5c734ed221ac1be96c5cb0e45a7061cbf2f2
Yaniv <syaniv1@gmail.com>
2025-12-12 14:27:56 +0200

    remove focus css

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -235,7 +235,7 @@ function AddTaskModal({ onClose, onAdd }) {
                                     setDays([0, 1, 2, 3, 4, 5, 6]);
                                 }
                             }}
-                            className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 ${doNotRepeat ? 'bg-green-500' : 'bg-gray-200'}`}
+                            className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${doNotRepeat ? 'bg-green-500' : 'bg-gray-200'}`}
                         >
                             <span
                                 className={`inline-block h-4 w-4 transform rounded-full bg-white transition-transform duration-200 ease-in-out ${doNotRepeat ? 'translate-x-6' : 'translate-x-1'}`}
